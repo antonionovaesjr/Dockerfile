68e9e9e2925f7fc3e4892c8d2effcd18a18b797a
Antonio <antonionovaesjr@gmail.com>
2019-06-10 12:58:55 -0300

    remoção de freeradius

--- Freeradius-Postgresl/freeradius/user-test.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO radcheck (id, username, attribute, op, value) VALUES (1,'joeuser','Cleartext-Password',':=','somepassword');
-INSERT INTO radreply  (username, attribute, op, value) VALUES ('joeuser', 'Mikrotik-Rate-Limit', '=', '512k/512k');
